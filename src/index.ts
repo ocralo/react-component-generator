5b22bd216dfb13bd09811c386f8b8db9d67b70aa
ocralo <ocralo@gmail.com>
2024-09-26 16:44:45 -0500

    feat: add colors in commands logs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
-import { input } from '@inquirer/prompts'
+import { program } from 'commander'
+import chalk from 'chalk'
 
-async function main() {
-	const answer = await input({ message: 'Enter your name' })
-	console.log(answer)
-}
+program
+	.version('1.0.0')
+	.description('My Node CLI')
+	.option('-n, --name <type>', 'Add your name')
+	.action((options) => {
+		console.log(chalk.blue(`Hey, ${options.name}!`))
+		console.log(chalk.green(`Hey, ${options.name}!`))
+		console.log(chalk.red(`Hey, ${options.name}!`))
+	})
 
-main()
+program.parse(process.argv)
