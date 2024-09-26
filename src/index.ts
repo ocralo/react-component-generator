9b6066e4ccab1f8ef9dc7cf7055c220ff0ac8daa
ocralo <ocralo@gmail.com>
2024-09-26 17:03:31 -0500

    feat: add title in app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
 import { program } from 'commander'
 import chalk from 'chalk'
+import figlet from 'figlet'
+
+console.log(
+	chalk.red(
+		figlet.textSync('R C G', {
+			horizontalLayout: 'universal smushing',
+			whitespaceBreak: true,
+			font: 'Larry 3D 2'
+		})
+	)
+)
 
 program
 	.version('1.0.0')
-	.description('My Node CLI')
+	.description('React Component Generator')
 	.option('-n, --name <type>', 'Add your name')
 	.action((options) => {
 		console.log(chalk.blue(`Hey, ${options.name}!`))
