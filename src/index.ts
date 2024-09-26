b952443f1a070edebd4570e3a35d2432e13e2f23
Rodrigo Escobar Lopez <ocralo@gmail.com>
2024-09-26 17:32:03 -0500

    Feat: add colors in commands logs, PR template and update README (#1)
    * feat: add colors in commands logs
    
    * feat: add title in app
    
    * feat: update readme and add commands and collaborators sections
    
    * feat: add PR template for github

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 #!/usr/bin/env node
-import { input } from '@inquirer/prompts'
+import { program } from 'commander'
+import chalk from 'chalk'
+import figlet from 'figlet'
 
-async function main() {
-	const answer = await input({ message: 'Enter your name' })
-	console.log(answer)
-}
+console.log(
+	chalk.red(
+		figlet.textSync('R C G', {
+			horizontalLayout: 'universal smushing',
+			whitespaceBreak: true,
+			font: 'Larry 3D 2'
+		})
+	)
+)
 
-main()
+program
+	.version('1.0.0')
+	.description('React Component Generator')
+	.option('-n, --name <type>', 'Add your name')
+	.action((options) => {
+		console.log(chalk.blue(`Hey, ${options.name}!`))
+		console.log(chalk.green(`Hey, ${options.name}!`))
+		console.log(chalk.red(`Hey, ${options.name}!`))
+	})
+
+program.parse(process.argv)
